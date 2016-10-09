1f14d470352d0b4ab7117191bf33fb1a04cf7d85
zithron <abdulbasithb70@gmail.com>
2016-10-09 15:32:24 +0700

    mau fix
    okeoeke

--- js/init.js
+++ js/init.js
@@ -1,17 +1,38 @@
-(function($){
-  $(function(){
+(function ($) {
+    $(function () {
 
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
-    var owl = $("#carousel-header .owl-carousel");
-	owl.owlCarousel({
-	    items:1,
-	    loop:true,
-	    margin:10,
-	    autoplay:true,
-	    dots:true,
-	    autoplayTimeout:5000,
-	    autoplayHoverPause:true
-	});
-  }); // end of document ready
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
+        // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+        $('.modal-trigger').leanModal();
+        $('.materialboxed').materialbox();
+        $('.dropdown-button').dropdown({
+            inDuration: 300,
+            outDuration: 225,
+            constrain_width: false, // Does not change width of dropdown to that of the activator
+            hover: true, // Activate on hover
+            gutter: 0, // Spacing from edge
+            belowOrigin: false, // Displays dropdown below the button
+            alignment: 'left' // Displays dropdown with edge aligned to the left of button
+        });
+
+        //carousel
+        var owl = $("#carousel-header .owl-carousel");
+        owl.owlCarousel({
+            items: 1,
+            loop: true,
+            margin: 10,
+            autoplay: true,
+            dots: true,
+            autoplayTimeout: 5000,
+            autoplayHoverPause: true
+        });
+
+        $(".collapsible-header").click(function () {
+            $(".zmdi-plus").hide();
+            $(".zmdi-minus").css({
+                "display": "inline"
+            });
+        });
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
