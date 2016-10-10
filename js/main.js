19d930621094220d3866204865dc246889a312a9
zithron <abdulbasithb70@gmail.com>
2016-10-10 09:04:49 +0700

    tinggal finishing
    bismillah

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    $('.collapsible-header').click(function () {
+        $('.zmdi').toggle(function () {
+            $('.zmdi-plus').css({
+                "display": "none"
+            });
+            $('.zmdi - minus').css({
+                "display": "inline"
+            });
+        });
+    });
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.fixed-action-btn').fadeIn();
+        } else {
+            $('.fixed-action-btn').fadeOut();
+        }
+    });
+
+    $('.fixed-action-btn').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+});
